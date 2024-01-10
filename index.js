7570efa23e21fefc861f09d933badb4361f4be1e
Kevin A <kraf4yt@gmail.com>
2024-01-10 10:50:58 -0700

    Added docker files

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express');
+const express = require("express");
+
+const boxes = [{ boxID: 1 }, { boxID: 2 }, { boxID: 3 }, { boxID: 4 }];
 
 const app = express();
 
-console.log("Hello");
+app.get("/boxes", (req, res) => {
+  res.send(JSON.stringify(boxes));
+});
+
+app.listen(3000);
